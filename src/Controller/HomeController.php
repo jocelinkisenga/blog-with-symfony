8e73b6ab0263c8a765b6c70b4a285554a449bdd7
jocelinkisenga <kisengajocelin7@gmail.com>
2022-05-17 16:57:09 +0200

    ajout des fichiers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController {
+class HomeController extends AbstractController {
 
-	#[Route('/')]
+	public string $nom = "jocelin";
+
+	
 	public function index (): Response {
 		
-		return new Response('name: Jocelin kisenga');
+		return $this->render('pages/index.html.twig',['name'=>$this->nom]);
 	}
 
 	#[Route('/detail/{slug}')]
